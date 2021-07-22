89200bf1d242e5268eace34558a550454dcf2d2e
anbunathan <anbunathan.r@gmail.com>
2021-07-22 20:23:25 +0530

    updated

--- data-visualization/config.js
+++ data-visualization/config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-  consumer_key:         'zlwdKs4qnl6cxisx33ZtfDLfy',
-  consumer_secret:      'YZJNtLKDItUeEiXt5xT9d6mDBi1YlMpPYqUyBdkb3JDE07RGtr',
-  access_token:         '144185944-6SpHl29v8W3XARPG1zAekOtbumWBLTqpThWWsF7r',
-  access_token_secret:  'jTgzgHZoiRz5bU7o2pIAcq0ijEiqfu5RdhYXMFONb3e7O',
+  consumer_key:         'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+  consumer_secret:      'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy',
+  access_token:         'zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz',
+  access_token_secret:  'mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm',
 }
